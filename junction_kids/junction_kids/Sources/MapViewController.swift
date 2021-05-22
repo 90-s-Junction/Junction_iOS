e7c3c4ff847a9389ec094256a54255ce122c1211
artificIn <djgmd1021@gmail.com>
2021-05-22 17:23:40 +0900

    Update map

---
+++ junction_kids/junction_kids/Sources/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  junction_kids
+//
+//  Created by 성다연 on 2021/05/22.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
